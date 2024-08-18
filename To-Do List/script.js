4143d40b219b04472ff17c64812b9e68631c8763
fashton28 <fashton502@gmail.com>
2024-08-17 22:32:37 -0400

    add remove functionality

--- To-Do List/script.js
+++ To-Do List/script.js
@@ -15,9 +15,40 @@ button.addEventListener("click",()=> {
         span.innerHTML = "\u00d7";
         li.appendChild(span);
 
+        span.addEventListener("click", function(){
+            li.remove();
+        })
 
     }
 
     input.value = " ";
+
+    
+
    
 })
+
+
+input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") { // Check if the pressed key is Enter
+        if (input.value === "") {
+            alert("You must write something!");
+        } else {  
+            let li = document.createElement("li");
+            let text = document.createTextNode(input.value);
+            li.appendChild(text);
+            ul.appendChild(li);
+
+            let span = document.createElement("span");
+            span.innerHTML = "\u00d7"; // "×" symbol for delete button
+            li.appendChild(span);
+
+            // Add event listener to the span to remove the list item
+            span.addEventListener("click", function() {
+                li.remove();
+            });
+
+            input.value = ""; // Clear the input field after adding the item
+        }
+    }
+});
