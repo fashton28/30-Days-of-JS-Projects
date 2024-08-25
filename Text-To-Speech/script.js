996d65d840e4b7c7315a26ec717a5cbb739542ba
fashton28 <fashton502@gmail.com>
2024-08-25 16:51:07 -0400

    starter files

--- Text-To-Speech/script.js
+++ Text-To-Speech/script.js
@@ -4,12 +4,13 @@
 
 function speak() {
     // Create a SpeechSynthesisUtterance
-    let input = document.getElementById("text")
-    const utterance = new SpeechSynthesisUtterance(`${input.value}`);
-  
+    let input = document.getElementById("text").value
+    
+    const utterance = new SpeechSynthesisUtterance(`${input}`);
+
     // Select a voice
     const voices = speechSynthesis.getVoices();
-    utterance.voice = voices[0]; // Choose a specific voice
+    utterance.voice = voices[1]; // Choose a specific voice
   
     // Speak the text
     
