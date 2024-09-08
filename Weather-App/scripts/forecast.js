f84ff315d7b85ef5a52563da33cad955a8817c9a
fashton28 <fashton502@gmail.com>
2024-09-07 21:43:18 -0400

    add weather condition to UI

--- Weather-App/scripts/forecast.js
+++ Weather-App/scripts/forecast.js
@@ -7,36 +7,42 @@ const getCity = async (city) => {
     const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
     const query = `?apikey=${key}&q=${city}`
     
-    const response = await fetch(base+query)
+    const response = await fetch(base+query);
 
     const data = await response.json();
-    return(data[0].Key)
+    return(data[0].Key);
 
 }
 
 const getWeather = async (code) => {
     const base = `http://dataservice.accuweather.com/currentconditions/v1/${code}`;
-    const query = `?apikey=${key}`
-    const response = await fetch(base+query)
-    const data = await response.json()
-    return (data[0].Temperature.Metric.Value);
+    const query = `?apikey=${key}`;
+    const response = await fetch(base+query);
+    const data = await response.json();
+    return (data[0]);
 }
 
 
 const form = document.querySelector("form");
-const temp = document.getElementById("temp")
+
+const temp = document.getElementById("temp");
+const title = document.querySelector("h5");
+const condition = document.querySelector(".condition");
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
         getCity(form.city.value.trim()).then(data =>{
             getWeather(data).then(result =>{
-                temp.innerHTML = `${result}`
+                condition.innerHTML = `${result.WeatherText}`;
+                temp.innerHTML = `${result.Temperature.Metric.Value}`;
+                title.innerHTML = `${form.city.value.trim()}`;
+                form.reset();
             })
         }).catch(err=>{
-            console.log("Could not fetch")
+            console.log("Could not fetch");
         })
 
-        form.reset()
+        
 
 
 
